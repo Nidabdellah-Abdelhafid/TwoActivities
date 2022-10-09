e985c2f4f996e1219e43d90d7e4d8838fcb5105b
Nidabdellah-Abdelhafid <nidabdellahabdelhafid99@gmail.com>
2022-10-09 19:09:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private Button send;
+    private EditText txtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtMessage = findViewById(R.id.editText_main);
+        send = findViewById(R.id.button_main);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = txtMessage.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("message", msg);
+                startActivity(intent);
+            }
+        });
+    }
+}
